1e3e2567325115cdd2d7e574e62d7de347792bd6
ludmila wagner <lu.wagner91@gmail.com>
2023-09-28 19:14:40 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import { useState, useEffect } from 'react'
+
+import './App.css'
+
+function App() {
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({x: 0, y: 0})
+
+
+useEffect(() => {
+
+  const handleMove = (event) => {
+    const { clientX, clientY } = event
+    console.log('handleMove', { clientX, clientY });
+    setPosition({ x: clientX, y: clientY})
+  }
+
+  if (enabled) {
+    window.addEventListener('pointermove', handleMove)
+  }
+
+  //cleanup 
+  //se ejecuta cuando el componente se desmonta 
+  //y cuando cambian las dependencias, antes de ejecutar el efecto de nuevo.
+return () => {
+  window.removeEventListener('pointermove', handleMove)//Limpiamos las subscripciones
+}
+}, [enabled])  
+  
+
+
+return (
+    <main>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: '#09f',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left:-20,
+        top:-20,
+        width: 40,
+        height: 40,
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }}/>
+     <button onClick={() => setEnabled(!enabled) }>{enabled ? 'Desactivar' : 'Activar'} seguir puntero</button>
+    </main>
+  )
+}
+
+export default App
